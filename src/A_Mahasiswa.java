b9c71f91d0233e0ef086febf58268a6953377b5a
Aidiladh <aidila1684211@gmail.com>
2022-03-28 16:58:44 +0700

    mengakses anggota kelas (member class)

---
+++ src/A_Mahasiswa.java
@@ -0,0 +1,12 @@
+public class A_Mahasiswa {
+    public int nrp;
+    public String nama;
+    public void info() {
+        System.out.println("lni siswa RPL Polbeng");
+    }
+    public static void main(String args[]) {
+        A_Mahasiswa it = new A_Mahasiswa();
+        it.nrp = 5;
+        it.nama = "Andi";
+        it.info();
+    }}
